c77d8202e1a24d0c4a433fef72f5128e205240ac
Shinhee Park <shinhee.park777@gmail.com>
2022-02-01 22:55:25 -0800

    Added initial animation on hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -4,6 +4,8 @@ const Hero = () => {
     return(
         <div className="hero">
             <h1>Palettes</h1>
+
+            <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_srwniiih.json"  background="transparent"  speed="1"  style={{width: "300px"}, {height: "300px"}}  loop  autoplay></lottie-player>
         </div>
     );
 };
