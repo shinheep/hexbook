a07839b2b6383cba114aac6ef370dbe8ae2bfbd7
Shinhee Park <shinhee.park777@gmail.com>
2022-02-02 22:26:01 -0800

    Added gradient to button

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
 
             <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_srwniiih.json"  background="transparent"  speed="1"  style={{width: "300px"}, {height: "300px"}}  loop  autoplay></lottie-player>
 
-            <Link activeClass="active" to="colors" spy={true} smooth={false}><button>Browse Colors</button></Link>
+            <Link activeClass="active" to="colors" spy={true} smooth={false}><button className="rainbow-box">Browse Colors</button></Link>
 
             
         </div>
