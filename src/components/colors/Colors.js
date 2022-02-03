c4a6e743306ea1f937fac48bb298f2d61367ca8d
Shinhee Park <shinhee.park777@gmail.com>
2022-02-02 22:38:24 -0800

    Added basic color blocks

--- src/components/colors/Colors.js
+++ src/components/colors/Colors.js
@@ -2,8 +2,61 @@ import "./colors.css"
 
 const Colors = () => {
     return(
-        <div className="colors">
+        <div className="mainColorsContainer">
             <h1>Colors</h1>
+        
+            <div className="colors">
+
+                <div className="colorContainer">
+                    <div className="colorBlock1"/>
+                    <div className="colorBlock2"/>
+                    <div className="colorBlock3"/>
+                    <div className="colorBlock4"/>
+                </div>
+
+                <div className="colorContainer">
+                    <div className="colorBlock1"/>
+                    <div className="colorBlock2"/>
+                    <div className="colorBlock3"/>
+                    <div className="colorBlock4"/>
+                </div>
+
+                <div className="colorContainer">
+                    <div className="colorBlock1"/>
+                    <div className="colorBlock2"/>
+                    <div className="colorBlock3"/>
+                    <div className="colorBlock4"/>
+                </div>
+
+                <div className="colorContainer">
+                    <div className="colorBlock1"/>
+                    <div className="colorBlock2"/>
+                    <div className="colorBlock3"/>
+                    <div className="colorBlock4"/>
+                </div>
+                
+                <div className="colorContainer">
+                    <div className="colorBlock1"/>
+                    <div className="colorBlock2"/>
+                    <div className="colorBlock3"/>
+                    <div className="colorBlock4"/>
+                </div>
+
+                <div className="colorContainer">
+                    <div className="colorBlock1"/>
+                    <div className="colorBlock2"/>
+                    <div className="colorBlock3"/>
+                    <div className="colorBlock4"/>
+                </div>
+
+                <div className="colorContainer">
+                    <div className="colorBlock1"/>
+                    <div className="colorBlock2"/>
+                    <div className="colorBlock3"/>
+                    <div className="colorBlock4"/>
+                </div>
+            </div>
+
         </div>
     );
 };
