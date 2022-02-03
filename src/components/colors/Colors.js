328e6981fa6234b49ac63813b821f65aec6b6da9
Shinhee Park <shinhee.park777@gmail.com>
2022-02-02 23:29:29 -0800

    Added different color palettes

--- src/components/colors/Colors.js
+++ src/components/colors/Colors.js
@@ -15,45 +15,45 @@ const Colors = () => {
                 </div>
 
                 <div className="colorContainer">
-                    <div className="colorBlock1"/>
-                    <div className="colorBlock2"/>
-                    <div className="colorBlock3"/>
-                    <div className="colorBlock4"/>
+                    <div className="colorBlock5"/>
+                    <div className="colorBlock6"/>
+                    <div className="colorBlock7"/>
+                    <div className="colorBlock8"/>
                 </div>
 
                 <div className="colorContainer">
-                    <div className="colorBlock1"/>
-                    <div className="colorBlock2"/>
-                    <div className="colorBlock3"/>
-                    <div className="colorBlock4"/>
+                    <div className="colorBlock9"/>
+                    <div className="colorBlock10"/>
+                    <div className="colorBlock11"/>
+                    <div className="colorBlock12"/>
                 </div>
 
                 <div className="colorContainer">
-                    <div className="colorBlock1"/>
-                    <div className="colorBlock2"/>
-                    <div className="colorBlock3"/>
-                    <div className="colorBlock4"/>
+                    <div className="colorBlock13"/>
+                    <div className="colorBlock14"/>
+                    <div className="colorBlock15"/>
+                    <div className="colorBlock16"/>
                 </div>
                 
                 <div className="colorContainer">
-                    <div className="colorBlock1"/>
-                    <div className="colorBlock2"/>
-                    <div className="colorBlock3"/>
-                    <div className="colorBlock4"/>
+                    <div className="colorBlock17"/>
+                    <div className="colorBlock18"/>
+                    <div className="colorBlock19"/>
+                    <div className="colorBlock20"/>
                 </div>
 
                 <div className="colorContainer">
-                    <div className="colorBlock1"/>
-                    <div className="colorBlock2"/>
-                    <div className="colorBlock3"/>
-                    <div className="colorBlock4"/>
+                    <div className="colorBlock21"/>
+                    <div className="colorBlock22"/>
+                    <div className="colorBlock23"/>
+                    <div className="colorBlock24"/>
                 </div>
 
                 <div className="colorContainer">
-                    <div className="colorBlock1"/>
-                    <div className="colorBlock2"/>
-                    <div className="colorBlock3"/>
-                    <div className="colorBlock4"/>
+                    <div className="colorBlock25"/>
+                    <div className="colorBlock26"/>
+                    <div className="colorBlock27"/>
+                    <div className="colorBlock28"/>
                 </div>
             </div>
 
