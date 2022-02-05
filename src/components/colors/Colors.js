56c2da2099a49992e74ddb42076b1857536f5775
Shinhee Park <shinhee.park777@gmail.com>
2022-02-05 14:39:30 -0800

    Automated color palette entry system

--- src/components/colors/Colors.js
+++ src/components/colors/Colors.js
@@ -24,10 +24,10 @@ const Colors = () => {
     const colorPalettes = palettes && palettes.map((palette, index) => {
         return(
              <div className="palette">
-                <div style={{background: `${palette.hex1}`, width: "400px", margin: "0 auto"}} >{palette.hex1}</div>
-                <div style={{background: `${palette.hex2}`, width: "400px", margin: "0 auto"}} >{palette.hex2}</div>
-                <div style={{background: `${palette.hex3}`, width: "400px", margin: "0 auto"}} >{palette.hex3}</div>
-                <div style={{background: `${palette.hex4}`, width: "400px", margin: "0 auto"}} >{palette.hex4}</div>
+                <div className="color" style={{background: `${palette.hex1}`, margin: "0 auto", borderTopLeftRadius: "20px", borderBottomLeftRadius: "20px"}} >{palette.hex1}</div>
+                <div className="color" style={{background: `${palette.hex2}`, margin: "0 auto"}} >{palette.hex2}</div>
+                <div className="color" style={{background: `${palette.hex3}`, margin: "0 auto"}} >{palette.hex3}</div>
+                <div className="color" style={{background: `${palette.hex4}`, margin: "0 auto", borderTopRightRadius: "20px", borderBottomRightRadius: "20px"}} >{palette.hex4}</div>
              </div>
         )
     })
@@ -40,58 +40,6 @@ const Colors = () => {
 
             {colorPalettes}
         
-            <div className="colors">
-
-                <div className="colorContainer">
-                    <div className="colorBlock1">asdf</div>
-                    <div className="colorBlock2"/>
-                    <div className="colorBlock3"/>
-                    <div className="colorBlock4"/>
-                </div>
-
-                <div className="colorContainer">
-                    <div className="colorBlock5"/>
-                    <div className="colorBlock6"/>
-                    <div className="colorBlock7"/>
-                    <div className="colorBlock8"/>
-                </div>
-
-                <div className="colorContainer">
-                    <div className="colorBlock9"/>
-                    <div className="colorBlock10"/>
-                    <div className="colorBlock11"/>
-                    <div className="colorBlock12"/>
-                </div>
-
-                <div className="colorContainer">
-                    <div className="colorBlock13"/>
-                    <div className="colorBlock14"/>
-                    <div className="colorBlock15"/>
-                    <div className="colorBlock16"/>
-                </div>
-                
-                <div className="colorContainer">
-                    <div className="colorBlock17"/>
-                    <div className="colorBlock18"/>
-                    <div className="colorBlock19"/>
-                    <div className="colorBlock20"/>
-                </div>
-
-                <div className="colorContainer">
-                    <div className="colorBlock21"/>
-                    <div className="colorBlock22"/>
-                    <div className="colorBlock23"/>
-                    <div className="colorBlock24"/>
-                </div>
-
-                <div className="colorContainer">
-                    <div className="colorBlock25"/>
-                    <div className="colorBlock26"/>
-                    <div className="colorBlock27"/>
-                    <div className="colorBlock28"/>
-                </div>
-            </div>
-
         </div>
     );
 };
